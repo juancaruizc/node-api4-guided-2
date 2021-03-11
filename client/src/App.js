52be1ee560e70fbd894222ee4b8ce76ec5a9926e
juancaruizc <juancruizcruz@gmail.com>
2021-03-11 11:58:53 -0700

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+function getUrl(path) {
+  if (process.env.NODE_ENV === 'development') {
+    return 'http://localhost:5000' + path;
+  } else {
+    return path;
+  }
+}
+
 function App() {
+  const [message, setMessage] = useState('hello web39');
+  useEffect(() => {
+    fetch(getUrl('/api/hello'))
+      .then((res) => {
+        res.json();
+      })
+      .then((resBody) => {
+        setMessage(resBody.message);
+      })
+      .catch((err) => {});
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <header className='App-header'>{message}</header>
     </div>
   );
 }
