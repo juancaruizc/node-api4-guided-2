cfa84670f66e66a6f1fb3d83370ebad47430eb9f
juancaruizc <juancruizcruz@gmail.com>
2021-03-11 10:43:40 -0700

    updates

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('web39 rules');
+require('dotenv').config();
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+// on Heroku machine, an env variable is called "NODE_ENV" -> "production"
+if (process.env.NODE_ENV === 'development') {
+  const cors = require('cors');
+  server.use(cors());
+}
